44e3d0e711cb73703e650928b3657c4d984230d8
Wilson Tomisin <tommywilson972@gmail.com>
2022-10-24 00:11:07 +0100

    Modified the main js file

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,50 +1,10 @@
-// alert("main.js loads...")
-
-
-// $(document).ready(function () {
-//    $("button").click(function(event) {
-//       event.preventDefault()
-//       var $form = $(".contain #container")
-//       var $info = {
-//          email:$("input[type = 'email']",$form).val(),
-//          password:$("input[type ='password']",$form).val()
-//       }
-//       //console.log($info);
-//       var $err = $('.js-error',$form)
-//       var $success = $('.js-success',$form)
-
-//       if ($info.email.length <=7 || $info.password.length <= 6 ) {
-//          $err.text('ENTER A VALID EMAIL ADDRESS or A MORE SECURE PASSWORD').show();
-//       }else{
-//          $err.hide()
-//          $success.text('SUCCESSFULL').show()
-//       }
-//       $.ajax({
-//          type: "POST",
-//          url: "./assests/js/test.html",
-//          //data: "data",
-//          //dataType: "dataType",
-//          success: function (response) {
-//             $(".container").html(response)
-//          }
-//       });
-//       return false
-//    })  
-// })
-
-
-
-
-
-
-
  $(document).ready(function()  {
-   $("form").submit(function(event) {
+   $("#regContainer").submit(function(event) {
       // console.log("submitted");
          //alert("form was submitted.")
          event.preventDefault();
    
-         var $form = $(".contain #container")
+         var $form = $(".contain #regContainer")
          var $data ={
             email:$("input[type='email']",$form).val(),
             password:$("input[type='password']",$form).val()
@@ -97,16 +57,5 @@
     
  })
 
-/*function  test(){
-   var options = {
-      method: 'POST',
 
-   }
-   fetch("http://localhost/new%20php/phploginprj/ajax/register.php",options).then((res)=>{
-      console.log(res);
-  res.json();
-   }).then((data)=>{
-      console.log(data);
-   });
- }*/
 
